c16b8b0e19621801a703a92aa7074ee9585c094b
EduNunes52 <carloseduardo.nunes.52@outlook.com>
2025-02-19 19:01:32 -0300

    Persistencia a dados

--- src/main/java/com/example/AS/Model/Produto.java
+++ src/main/java/com/example/AS/Model/Produto.java
@@ -1,22 +1,41 @@
 package com.example.AS.Model;
 
 
+import jakarta.persistence.*;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+
+@Data
+@Entity
 public class Produto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+
+    @Column
     private String nome;
+
+    @Column
     private double preco;
 
 
     public Produto(int id, String nome, double preco) {
+
+
         this.id = id;
+
+
         this.nome = nome;
+
+
         this.preco = preco;
+
+
     }
     public int getId() {
         return id;
